9301e256a30da727e61271094a100939b11e31b4
ncosentino <n.b.cosentino@gmail.com>
2018-05-20 12:58:41 -0400

    Add Main Menu
    - Adding a main menu scene with a background...
    - New game button, exit game button.
    - New game button loads the game world.

---
+++ Assets/Scripts/Gui/MainMenu/MainMenuBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class MainMenuBehaviour : MonoBehaviour
+{
+    public void NewGameClick()
+    {
+        SceneManager.LoadScene("GameWorld");
+    }
+
+    public void ExitClick()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
