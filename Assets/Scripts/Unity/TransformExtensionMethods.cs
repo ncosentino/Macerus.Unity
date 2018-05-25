8a543d4d9ecc978cd4d461cec8ff88ef1fe17c75
ncosentino <n.b.cosentino@gmail.com>
2018-05-24 20:00:11 -0400

    Add TransformExtensionMethods for... Extensions!
    - Added some extensions for more obviously asking transforms for children.

---
+++ Assets/Scripts/Unity/TransformExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Unity
+{
+    public static class TransformExtensionMethods
+    {
+        public static IEnumerable<object> GetChildren(this Transform transform)
+        {
+            return transform.Cast<object>();
+        }
+
+        public static IEnumerable<GameObject> GetChildGameObjects(this Transform transform)
+        {
+            return transform
+                .GetChildren()
+                .Where(x => x is Transform)
+                .Cast<Transform>()
+                .Select(x => x.gameObject);
+        }
+    }
+}
