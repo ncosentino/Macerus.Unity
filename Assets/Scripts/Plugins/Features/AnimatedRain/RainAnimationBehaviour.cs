3c4f0ab2f4d5485edaf828971b42d212e4280ae8
ncosentino <n.b.cosentino@gmail.com>
2018-06-28 00:23:35 -0400

    [Fix] Accidentally Botched Animated Rain

--- Assets/Scripts/Plugins/Features/AnimatedRain/RainAnimationBehaviour.cs
+++ Assets/Scripts/Plugins/Features/AnimatedRain/RainAnimationBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using ProjectXyz.Framework.Contracts;
+using UnityEngine;
 
 namespace Assets.Scripts.Plugins.Features.AnimatedRain
 {
@@ -7,17 +8,24 @@ namespace Assets.Scripts.Plugins.Features.AnimatedRain
         public float ScrollSpeed;
         public float VerticalTileSize;
 
-        private Vector3 startPosition;
+        private Vector3 _startPosition;
 
         private void Start()
         {
-            startPosition = transform.position;
+            Contract.Requires(
+                ScrollSpeed >= 0,
+                $"{nameof(ScrollSpeed)} was not set to a non-negative value on '{gameObject}.{this}'.");
+            Contract.Requires(
+                VerticalTileSize > 0,
+                $"{nameof(VerticalTileSize)} was not set to a positive value on '{gameObject}.{this}'.");
+
+            _startPosition = transform.position;
         }
 
         private void Update()
         {
             float newPosition = Mathf.Repeat(Time.time * ScrollSpeed, VerticalTileSize);
-            transform.position = startPosition + Vector3.down * newPosition;
+            transform.position = _startPosition + Vector3.down * newPosition;
         }
     }
 }
