b0c9e057faa2debb30c454c4f28ca96c7b151289
ncosentino <n.b.cosentino@gmail.com>
2020-08-06 01:59:43 -0400

    RPG-16: Base Light Radius Info From Stats
    - Use stats instead of light radius behavior

--- Assets/Scripts/Plugins/Features/Actors/Interceptors/LightRadiusBehaviorInterceptor.cs
+++ Assets/Scripts/Plugins/Features/Actors/Interceptors/LightRadiusBehaviorInterceptor.cs
@@ -1,8 +1,13 @@
-﻿using System.Linq;
-using Assets.Scripts.Scenes.Explore.GameObjects;
+﻿using Assets.Scripts.Scenes.Explore.GameObjects;
 using Assets.Scripts.Unity.Resources;
+
+using ProjectXyz.Api.Enchantments;
+using ProjectXyz.Api.Framework.Entities;
 using ProjectXyz.Api.GameObjects;
-using ProjectXyz.Plugins.Features.CommonBehaviors.Api;
+using ProjectXyz.Plugins.Enchantments.Stats;
+using ProjectXyz.Plugins.Features.GameObjects.StatCalculation.Api;
+using ProjectXyz.Shared.Framework;
+
 using UnityEngine;
 
 namespace Assets.Scripts.Plugins.Features.Actors.Interceptors
@@ -10,23 +15,43 @@ namespace Assets.Scripts.Plugins.Features.Actors.Interceptors
     public sealed class LightRadiusBehaviorInterceptor : IGameObjectBehaviorInterceptor
     {
         private readonly IPrefabCreator _prefabCreator;
+        private readonly IStatCalculationService _statCalculationService;
 
-        public LightRadiusBehaviorInterceptor(IPrefabCreator prefabCreator)
+        public LightRadiusBehaviorInterceptor(
+            IPrefabCreator prefabCreator,
+            IStatCalculationService statCalculationService)
         {
             _prefabCreator = prefabCreator;
+            _statCalculationService = statCalculationService;
         }
 
         public void Intercept(
             IGameObject gameObject, 
             GameObject unityGameObject)
         {
-            var lightRadiusBehavior = gameObject
-                .Get<ILightRadiusBehavior>()
-                .SingleOrDefault();
-            if (lightRadiusBehavior == null)
-            {
-                return;
-            }
+            var context = new StatCalculationContext(
+                new IComponent[0],
+                new IEnchantment[0]);
+            var lightRadiusRadius = _statCalculationService.GetStatValue(
+                gameObject,
+                new StringIdentifier("Light Radius Radius"),
+                context);
+            var lightRadiusIntensity = _statCalculationService.GetStatValue(
+                gameObject,
+                new StringIdentifier("Light Radius Intensity"),
+                context);
+            var lightRadiusRed = _statCalculationService.GetStatValue(
+                gameObject,
+                new StringIdentifier("Light Radius Red"),
+                context);
+            var lightRadiusGreen = _statCalculationService.GetStatValue(
+                gameObject,
+                new StringIdentifier("Light Radius Green"),
+                context);
+            var lightRadiusBlue = _statCalculationService.GetStatValue(
+                gameObject,
+                new StringIdentifier("Light Radius Blue"),
+                context);
 
             var lightRadiusObject = _prefabCreator.Create<GameObject>("Mapping/Prefabs/LightRadius");
             lightRadiusObject.transform.position = new Vector3(0, 0, -1);
@@ -34,11 +59,11 @@ namespace Assets.Scripts.Plugins.Features.Actors.Interceptors
 
             var lightComponent = lightRadiusObject.GetComponent<Light>();
             lightComponent.color = new Color(
-                (float)lightRadiusBehavior.Red,
-                (float)lightRadiusBehavior.Green,
-                (float)lightRadiusBehavior.Blue);
-            lightComponent.range = (float)lightRadiusBehavior.Radius;
-            lightComponent.intensity = (float)lightRadiusBehavior.Intensity;
+                (float)lightRadiusRed,
+                (float)lightRadiusGreen,
+                (float)lightRadiusBlue);
+            lightComponent.range = (float)lightRadiusRadius;
+            lightComponent.intensity = (float)lightRadiusIntensity;
         }
     }
 }
