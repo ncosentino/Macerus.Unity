ad0b6c7162a0b98b47d89f355da7ad92ed032394
ncosentino <n.b.cosentino@gmail.com>
2021-02-10 21:16:43 -0800

    Template Create vs Load on Game Object Repos
    - The player actor is now "made from a template" (it's still faked)
    - Example started for loading a wall up, but there's no repo yet that does it.

--- Assets/Scripts/Scenes/Explore/GameObjects/TiledNet/TiledNetGameObjectRepository.cs
+++ Assets/Scripts/Scenes/Explore/GameObjects/TiledNet/TiledNetGameObjectRepository.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Assets.Scripts.TiledNet;
 using ProjectXyz.Api.Framework;
 using ProjectXyz.Api.GameObjects;
+using ProjectXyz.Api.Logging;
 using ProjectXyz.Shared.Framework;
 
 namespace Assets.Scripts.Scenes.Explore.GameObjects.TiledNet
@@ -12,13 +14,16 @@ namespace Assets.Scripts.Scenes.Explore.GameObjects.TiledNet
     {
         private readonly ITiledMapLoader _tiledMapLoader;
         private readonly IMacerusObjectRepository _gameObjectRepository;
+        private readonly ILogger _logger;
 
         public TiledNetGameObjectRepository(
             ITiledMapLoader tiledMapLoader,
-            IMacerusObjectRepository gameObjectRepository)
+            IMacerusObjectRepository gameObjectRepository,
+            ILogger logger)
         {
             _tiledMapLoader = tiledMapLoader;
             _gameObjectRepository = gameObjectRepository;
+            _logger = logger;
         }
 
         public IEnumerable<IGameObject> LoadForMap(IIdentifier mapId)
@@ -29,9 +34,47 @@ namespace Assets.Scripts.Scenes.Explore.GameObjects.TiledNet
             {
                 foreach (var tiledMapObject in tiledMapObjectLayer.Objects)
                 {
+                    var properties = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase)
+                    {
+                        ["X"] = tiledMapObject.X,
+                        ["Y"] = tiledMapObject.Y,
+                        ["Width"] = tiledMapObject.Width,
+                        ["Height"] = tiledMapObject.Height,
+                        ["$TiledId"] = tiledMapObject.Id,
+                        ["$TiledType"] = tiledMapObject.Type,
+                    };
+                    foreach (var property in tiledMapObject.Properties)
+                    {
+                        properties[property.Key.TrimStart('$')] = property.Value;
+                    }
+
+                    if (!properties.TryGetValue("TypeId", out var typeId))
+                    {
+                        throw new InvalidOperationException(
+                            $"No type ID found on tiled object '{tiledMapObject.Id}' " +
+                            $"on map '{mapId}'.");
+                    }
+
+                    if (properties.TryGetValue("TemplateId", out var templateId))
+                    {
+                        var gameObjectFromTemplate = _gameObjectRepository.CreateFromTemplate(
+                            new StringIdentifier(typeId.ToString()), // FIXME: assuming string is a bit hacky
+                            new StringIdentifier(templateId.ToString()), // FIXME: assuming string is a bit hacky
+                            properties);
+                        yield return gameObjectFromTemplate;
+                        continue;
+                    }
+
+                    if (!properties.TryGetValue("UniqueId", out var uniqueId))
+                    {
+                        throw new InvalidOperationException(
+                            $"No unique ID found on tiled object '{tiledMapObject.Id}' " +
+                            $"on map '{mapId}'.");
+                    }
+
                     var gameObject = _gameObjectRepository.Load(
-                        new StringIdentifier(tiledMapObject.Type),
-                        new StringIdentifier(tiledMapObject.Name));
+                        new StringIdentifier(typeId.ToString()), // FIXME: assuming string is a bit hacky
+                        new StringIdentifier(uniqueId.ToString())); // FIXME: assuming string is a bit hacky
                     yield return gameObject;
                 }
             }
