e13c3e81ed93c5ddb7016fe7bbe5da8ce2ce8072
ncosentino <n.b.cosentino@gmail.com>
2021-03-07 17:15:21 -0800

    Use Noesis GUI!
    - New MainMenu w/ Noesis GUI.

--- Assets/Scripts/Scenes/MainMenu/MainMenuSetup.cs
+++ Assets/Scripts/Scenes/MainMenu/MainMenuSetup.cs
@@ -1,5 +1,11 @@
 ﻿
 using Assets.Scripts.Console;
+using Assets.Scripts.Gui;
+using Assets.Scripts.Scenes.MainMenu.Gui.Views.MainMenu;
+using Assets.Scripts.Scenes.MainMenu.Input;
+
+using ProjectXyz.Framework.ViewWelding.Api;
+using ProjectXyz.Framework.ViewWelding.Api.Welders;
 
 using UnityEngine;
 
@@ -7,6 +13,23 @@ namespace Assets.Scripts.Scenes.MainMenu
 {
     public sealed class MainMenuSetup : IMainMenuSetup
     {
+        private readonly IGuiPrefabCreator _guiPrefabCreator;
+        private readonly IViewWelderFactory _viewWelderFactory;
+        private readonly IMainMenuView _mainMenuView;
+        private readonly IGuiInputStitcher _guiInputStitcher;
+
+        public MainMenuSetup(
+            IGuiPrefabCreator guiPrefabCreator,
+            IViewWelderFactory viewWelderFactory,
+            IMainMenuView mainMenuView,
+            IGuiInputStitcher guiInputStitcher)
+        {
+            _guiPrefabCreator = guiPrefabCreator;
+            _viewWelderFactory = viewWelderFactory;
+            _mainMenuView = mainMenuView;
+            _guiInputStitcher = guiInputStitcher;
+        }
+
         public void Setup()
         {
             var consoleObject = new GameObject()
@@ -14,6 +37,12 @@ namespace Assets.Scripts.Scenes.MainMenu
                 name = "ConsoleCommands",
             };
             consoleObject.AddComponent<GlobalConsoleCommandsBehaviour>();
+
+            var gui = _guiPrefabCreator.Create();
+            _viewWelderFactory
+                .Create<ISimpleWelder>(gui, _mainMenuView)
+                .Weld();
+            _guiInputStitcher.Attach(gui.GameObject);
         }
     }
 }
--- Assets/Scripts/Scenes/MainMenu/OptionsMenuBehaviour.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace Assets.Scripts.Scenes.MainMenu
-{
-    public sealed class OptionsMenuBehaviour : MonoBehaviour
-    {
-    }
-}
