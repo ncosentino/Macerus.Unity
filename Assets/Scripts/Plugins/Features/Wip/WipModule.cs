4965f331bbca6298ac8c2326f888a2f73cb971a2
ncosentino <n.b.cosentino@gmail.com>
2021-04-05 08:25:21 -0700

    RPG-183: Create primitive attack enemy combat AI
    - Adding a CombatTurnLogger just to visualize what's happening for now :)

--- Assets/Scripts/Plugins/Features/Wip/WipModule.cs
+++ Assets/Scripts/Plugins/Features/Wip/WipModule.cs
@@ -18,12 +18,14 @@ using ProjectXyz.Api.GameObjects;
 using ProjectXyz.Api.Logging;
 using ProjectXyz.Api.Stats;
 using ProjectXyz.Plugins.Features.Behaviors.Filtering.Default.Attributes;
+using ProjectXyz.Plugins.Features.Combat.Api;
 using ProjectXyz.Plugins.Features.CommonBehaviors;
 using ProjectXyz.Plugins.Features.CommonBehaviors.Api;
 using ProjectXyz.Plugins.Features.GameObjects.Actors.Api;
 using ProjectXyz.Plugins.Features.GameObjects.Enchantments.Default.Calculations;
 using ProjectXyz.Plugins.Features.GameObjects.Skills;
 using ProjectXyz.Plugins.Features.GameObjects.StatCalculation.Api;
+using ProjectXyz.Plugins.Features.TurnBased.Api;
 using ProjectXyz.Shared.Framework;
 
 namespace Assets.Scripts.Plugins.Features.Wip
@@ -36,6 +38,33 @@ namespace Assets.Scripts.Plugins.Features.Wip
                 .RegisterType<PlayerTestingBehaviorsInterceptor>()
                 .AsImplementedInterfaces()
                 .SingleInstance();
+            builder
+                .RegisterType<CombatTurnLogger>()
+                .AutoActivate()
+                .AsSelf();
+        }
+    }
+
+    public sealed class CombatTurnLogger
+    {
+        public CombatTurnLogger(
+            ICombatTurnManager combatTurnManager,
+            ILogger logger)
+        {
+            combatTurnManager.CombatStarted += (_, e) =>
+            {
+                logger.Info(
+                    $"Combat started.\r\n" +
+                    $"Turn Order:\r\n" +
+                    $"{string.Join("\r\n", e.ActorOrder.Select(x => "\t" + x))}");
+            };
+            combatTurnManager.TurnProgressed += (_, e) =>
+            {
+                logger.Info(
+                    $"Combat turn progressed.\r\n" +
+                    $"Last Turn: {e.ActorTurnProgression.Single()}\r\n" + 
+                    $"Next Turn: {e.ActorWithNextTurn}");
+            };
         }
     }
 
