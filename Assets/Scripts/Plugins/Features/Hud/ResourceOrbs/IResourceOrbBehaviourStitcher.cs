bfa0d0b85bffbb63b30a079c0dcf9b7b4411293d
ncosentino <n.b.cosentino@gmail.com>
2021-04-26 11:36:34 -0700

    Blow Away Old Unity-Specific HUD

--- Assets/Scripts/Plugins/Features/Hud/ResourceOrbs/IResourceOrbBehaviourStitcher.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProjectXyz.Api.Framework;
-
-namespace Assets.Scripts.Plugins.Features.Hud.ResourceOrbs
-{
-    public interface IResourceOrbBehaviourStitcher
-    {
-        IReadOnlyResourceOrbBehaviour Stitch(IResourceOrbPrefab resourceOrbPrefab, IIdentifier currentStatDefinitionId, IIdentifier maximumStatDefinitionId);
-    }
-}
